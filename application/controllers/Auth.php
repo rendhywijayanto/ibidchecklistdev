bbe4ac2e2ae32f6614b48499ee322a08d46328a0
Harfi Novian <harfinovian@gmail.com>
2017-08-22 08:49:56 +0700

    change auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -15,15 +15,15 @@ class Auth extends CI_Controller {
 		if($method != 'POST'){
 			json_output(400,array('status' => 400,'message' => 'Bad request.'));
 		} else {
-			$check_auth_client = $this->AuthModel->check_auth_client();
-			if($check_auth_client == true){
+//			$check_auth_client = $this->AuthModel->check_auth_client();
+//			if($check_auth_client == true){
 				$params = json_decode(file_get_contents('php://input'), TRUE);
 		        $email = $params['email'];
 		        $password = $params['password'];
 
 		        $response = $this->AuthModel->login($email,$password);
 				echo json_encode($response);
-			}
+//			}
 		}
 	}
 
